a7087aa70e62261889aaa5069180776a0d785d01
elinln <linneaelinnilsson@gmail.com>
2021-09-21 14:55:32 +0200

    base payment

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
+require('dotenv').config('.env');
+
+const secretKey = process.env.STRIPE_SECRET_KEY;
 const express = require('express');
+const stripe = require('stripe')(secretKey);
+
 const app = express();
-const env = require('dotenv');
 
-
-const config = env.config('.env');
-
-process.env
-console.log()
+app.get('/api', (req, res) => {
+    res.status(200).send("helloooo");
+})
 
 app.use(express.static('public'))
 
 
-
-app.get('/', (req, res) => {
-    res.status(200).send("helloooo");
+app.post("/api/session/new", async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                description: "en god parfym",
+                price_data: {
+                    currency: "sek",
+                    product_data: {
+                        name: "parfym"
+                    },
+                    unit_amount: 129900
+                },
+                quantity: 1
+            }
+        ],
+        mode: "payment",
+        success_url: "http://localhost:3000/success_checkout.html",
+        cancel_url: "http://localhost:3000/index.html"
+    });
+    res.status(200).json({ id: session.id })
 })
 
 
+
 app.listen(3000, () => {
-    console.log("up and running!");
+    console.log("server is running!");
 })
